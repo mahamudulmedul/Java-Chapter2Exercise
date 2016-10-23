5a6eeb468c858bff6a2316e74229b9b519b04235
Mahamudul Haque Medul <mahmudulmedul@gmail.com>
2016-10-23 13:32:20 +0600

    Add files via upload

---
+++ PrintingTable2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package printingtable2;
+
+/**
+ *
+ * @author Medul
+ */
+public class PrintingTable2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("a     b       pow(a,b)");
+        System.out.println("1     2       1");
+        System.out.println("2     3       8");
+        System.out.println("3     4       81");
+        System.out.println("4     5       1024");
+        System.out.println("5     6       15625");
+    }
+
+}
