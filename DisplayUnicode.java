1938f3dfd0307fb1099a04a103e41eed1ab83894
Mahamudul Haque Medul <mahmudulmedul@gmail.com>
2016-10-23 14:47:54 +0600

    Add files via upload

---
+++ DisplayUnicode.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package displayunicode;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Medul
+ */
+public class DisplayUnicode {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JOptionPane.showMessageDialog(null, JOptionPane.INFORMATION_MESSAGE);
+    }
+    
+}
